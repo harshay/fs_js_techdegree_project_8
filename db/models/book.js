2a2e3a4d0dd1e90844d355a13c875b8bb3c3c8d1
harshay <harshaykr@hotmail.com>
2020-08-15 21:07:30 +0100

    update

---
+++ db/models/book.js
@@ -0,0 +1,22 @@
+const Sequelize  = require('sequelize');
+const { INTEGER } = require('sequelize'); 
+
+module.exports = (sequelize) => {
+
+
+    class Book extends Sequelize.Model{}
+
+    Book.init({
+
+        id:{type: Sequelize.INTEGER, primarykey = true, autoIncrement: true},
+        title:{type: Sequelize.VARCHAR, allowNull: false}, 
+        author:{type: Sequelize.VARCHAR, allowNull: false}, 
+        genre:{type: Sequelize.VARCHAR, allowNull: false}, 
+        year:{type: Sequelize.INTEGER}
+
+    },{});
+
+    return Book; 
+
+}; 
+
